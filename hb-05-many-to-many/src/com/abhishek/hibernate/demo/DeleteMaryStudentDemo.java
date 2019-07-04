5119d37920aa0e816fb9c4fee8ac1375355584df
Abhishek <abhi.jain.0820@gmail.com>
2019-07-04 20:35:33 +0530

    Hibernate Advanced Mappings - ManyToMany

---
+++ hb-05-many-to-many/src/com/abhishek/hibernate/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,56 @@
+package com.abhishek.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.abhishek.hibernate.demo.entity.Course;
+import com.abhishek.hibernate.demo.entity.Instructor;
+import com.abhishek.hibernate.demo.entity.InstructorDetail;
+import com.abhishek.hibernate.demo.entity.Review;
+import com.abhishek.hibernate.demo.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the student Mary from database
+			int studentId = 2;
+			Student tempStudent = session.get(Student.class, studentId);
+			
+			System.out.println("\nLoaded Student: " + tempStudent);
+			System.out.println("Course(s): " + tempStudent.getCourses());
+			
+			// delete student
+			System.out.println("\nDeleting student: " + tempStudent);
+			session.delete(tempStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
